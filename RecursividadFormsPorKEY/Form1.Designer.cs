36174821a7ca567777b9c60b8c0b9891c0875fe8
JoseFabela <jfabela522@gmail.com>
2023-12-13 14:42:23 -0600

    Agregar archivos de proyecto.

---
+++ RecursividadFormsPorKEY/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RecursividadFormsPorKEY
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.explore = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // explore
+            // 
+            this.explore.Location = new System.Drawing.Point(537, 71);
+            this.explore.Name = "explore";
+            this.explore.Size = new System.Drawing.Size(75, 23);
+            this.explore.TabIndex = 0;
+            this.explore.Text = "Explorar";
+            this.explore.UseVisualStyleBackColor = true;
+            this.explore.Click += new System.EventHandler(this.explore_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(34, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(458, 212);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.explore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button explore;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ RecursividadFormsPorKEY/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecursividadFormsPorKEY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void ListFilesRecursively(string directoryPath)
+        {
+            try
+            {
+                foreach (string file in Directory.GetFiles(directoryPath))
+                {
+                    listBox1.Items.Add($"Archivo: {Path.GetFileName(file)}, Extensión: {Path.GetExtension(file)}");
+                }
+
+                foreach (string subdirectory in Directory.GetDirectories(directoryPath))
+                {
+                    ListFilesRecursively(subdirectory);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al acceder a {directoryPath}: {ex.Message}");
+            }
+        }
+
+        private void explore_Click_1(object sender, EventArgs e)
+        {
+            string directoryPath = @"C:\Users\junio\OneDrive\Escritorio"; // Cambia esto a la ruta del directorio que desees explorar
+            listBox1.Items.Clear();
+            ListFilesRecursively(directoryPath);
+        }
+    }
+}
